fe06be1b03b84d9eaaf021b134a3f8514b164e00
ronnie <ronnieyeung@fontrip.com>
2022-11-26 10:39:20 +0800

    add global typography

---
+++ src/utilities/typography.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const fontSize = {
+  h5: "20px",
+  title: "16px",
+  subtitle: "14px",
+  p1: "16px",
+  p2: "14px",
+  caption: "12px",
+};
+
+export const H5 = styled.h3`
+  font-size: ${fontSize.h5};
+  line-height: ${(props) => (props.mobile ? "30px" : "20px")};
+  font-weight: ${(props) => (props.medium ? "500" : "700")};
+`;
+
+export const Title = styled.h4`
+  font-size: ${fontSize.title};
+  line-height: ${(props) => (props.mobile ? "24px" : "20px")};
+  font-weight: ${(props) => (props.bold ? "700" : "400")};
+`;
+
+export const Subtitle = styled.h5`
+  font-size: ${fontSize.subtitle};
+  line-height: ${(props) => (props.mobile ? "24px" : "18px")};
+  font-weight: ${(props) => (props.bold ? "bold" : "400")};
+`;
+
+export const P1 = styled.p`
+  font-size: ${fontSize.p1};
+  line-height: ${(props) => (props.mobile ? "24px" : "20px")};
+  font-weight: ${(props) => (props.medium ? "500" : "400")};
+`;
+
+export const P2 = styled.p`
+  font-size: ${fontSize.p2};
+  line-height: ${(props) => (props.mobile ? "24px" : "20px")};
+  font-weight: ${(props) => (props.medium ? "500" : "400")};
+`;
+
+export const Caption = styled.p`
+  font-size: ${fontSize.caption};
+  line-height: ${(props) => (props.mobile ? "18px" : "16px")};
+  font-weight: ${(props) => (props.bold ? "700" : "400")};
+`;
