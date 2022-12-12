38d85a796f715208c045e80011c91075f02802ee
ronnie <ronnieyeung@fontrip.com>
2022-12-12 23:13:30 +0800

    add sign by myself page

--- src/component/SignDocs/Own/SignByMyself.js
+++ src/component/SignDocs/Own/SignByMyself.js
@@ -1,20 +1,26 @@
-import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import { colors } from "../../../utilities/colors";
 import { BtnStyle } from "../../../utilities/layout";
 import { Subtitle } from "../../../utilities/typography";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { useState } from "react";
+import { UploadDocs } from "../Own/UploadDocs";
+import { NameDocs } from "./NameDocs";
+import { SignDoc } from "./SignDocs";
+import { DocHeader } from "./DocHeader";
 
 const SignNavHeader = styled.header`
   background-color: ${colors.n1};
   box-shadow: 0 0 15px ${colors.shadowlv1};
-  padding: 16px 0;
+  position: fixed;
+  z-index: 10;
+  width: 100%;
 `;
 
 const SignNav = styled.div`
   display: flex;
   justify-content: space-between;
+  padding: 16px 32px;
 `;
 
 const NavLinks = styled.div`
@@ -54,40 +60,61 @@ const NavStep = styled.div`
 `;
 
 export const SignByMyself = () => {
+  const [pdf, setPdf] = useState(null);
+  const [fileName, setFileName] = useState(null);
+  const [docName, setDocName] = useState(null);
+  const [docTags, setDocTags] = useState(null);
+
   return (
     <>
       <SignNavHeader>
-        <Container>
-          <SignNav>
-            <NavLinks>
-              <NavLink>
-                <NavStep active>1</NavStep>
-                <Subtitle $mode="active">上傳檔案</Subtitle>
-              </NavLink>
-              <NavBlock />
-              <NavLink>
-                <NavStep>2</NavStep>
-                <Subtitle>確認上傳檔案</Subtitle>
-              </NavLink>
-              <NavBlock />
-              <NavLink>
-                <NavStep>3</NavStep>
-                <Subtitle>製作簽署檔案</Subtitle>
-              </NavLink>
-              <NavBlock />
-              <NavLink>
-                <NavStep>4</NavStep>
-                <Subtitle>下載簽署檔案</Subtitle>
-              </NavLink>
-            </NavLinks>
-            <NavHelp>
-              <AiOutlineQuestionCircle color={colors.n5} size="2rem" />
-              <BtnStyle>取消</BtnStyle>
-            </NavHelp>
-          </SignNav>
-        </Container>
+        <SignNav>
+          <NavLinks>
+            <NavLink>
+              <NavStep active>1</NavStep>
+              <Subtitle $mode="active">上傳檔案</Subtitle>
+            </NavLink>
+            <NavBlock />
+            <NavLink>
+              <NavStep>2</NavStep>
+              <Subtitle>確認上傳檔案</Subtitle>
+            </NavLink>
+            <NavBlock />
+            <NavLink>
+              <NavStep>3</NavStep>
+              <Subtitle>製作簽署檔案</Subtitle>
+            </NavLink>
+            <NavBlock />
+            <NavLink>
+              <NavStep>4</NavStep>
+              <Subtitle>下載簽署檔案</Subtitle>
+            </NavLink>
+          </NavLinks>
+          <NavHelp>
+            <AiOutlineQuestionCircle color={colors.n5} size="1.5rem" />
+            <BtnStyle>取消</BtnStyle>
+          </NavHelp>
+        </SignNav>
+        {docName !== null && <DocHeader docName={docName} docTags={docTags} />}
       </SignNavHeader>
-      <Outlet />
+
+      {pdf === null ? (
+        <UploadDocs setPdf={setPdf} setFileName={setFileName} />
+      ) : docName === null ? (
+        <NameDocs
+          setPdf={setPdf}
+          fileName={fileName}
+          setDocName={setDocName}
+          setDocTags={setDocTags}
+        />
+      ) : (
+        <SignDoc
+          pdf={pdf}
+          docName={docName}
+          setDocName={setDocName}
+          docTags={docTags}
+        />
+      )}
     </>
   );
 };
