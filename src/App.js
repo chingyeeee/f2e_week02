3bd252dd43a7750f6be5cce3772655e3fbf45be8
ronnie <ronnieyeung@fontrip.com>
2022-12-12 23:16:08 +0800

    add sign route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Home } from "./component/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Member } from "./component/Member";
 import { SignByMyself } from "./component/SignDocs/Own/SignByMyself";
-import { UploadDocs } from "./component/SignDocs/Own/UploadDocs";
 
 function App() {
   return (
@@ -11,9 +10,7 @@ function App() {
       <Routes>
         <Route index path="/login" element={<Home />} />
         <Route path="/" element={<Member />} />
-        <Route path="/sign-by-myself" element={<SignByMyself />}>
-          <Route index element={<UploadDocs />} />
-        </Route>
+        <Route path="/sign-by-myself" element={<SignByMyself />}></Route>
       </Routes>
     </Router>
   );
